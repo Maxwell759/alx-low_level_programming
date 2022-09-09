89a0b881725ea7374d2bf5d9096d6d1108149ca5
maxwell759 <Juventinus90@gmail.com>
2022-09-09 13:51:58 +0100

    entry point of programs

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,27 +1,26 @@
-#include <stdlib.h>	
-#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdlib.h>
+#include <time.h>
+/**
+*main - entry point of programs
+*
+*Return: return 0
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	
-	if (n > 0)
+	if (n < 0)
 	{
-	printf("is positive\n");
+		printf("%d is negative\n", n);
 	}
-	else if (n==0)
+	else if (n > 0)
 	{
-	printf("is zero\n");
+		printf("%d is positive\n", n);
 	}
-	else{
-	printf("is negative\n");
-	}
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
