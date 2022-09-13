7f0e77cf3a0db22af6fac7e9c2f1b18c836fdb64
maxwell759 <Juventinus90@gmail.com>
2022-09-13 19:06:16 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*print_alphabet_x10 - checks for lowercase character
+* 
+*desc: The function uses loop to operate
+*Return: no return
+*/
+
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+return (1);
+
+return (0);	
+}
