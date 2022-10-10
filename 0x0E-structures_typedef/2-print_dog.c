48f030254a24f707401132d01afff524fd9b9b19
maxwell <59886894+Maxwell759@users.noreply.github.com>
2022-10-10 23:39:22 +0100

    Hot fix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,24 +10,24 @@
 
 void print_dog(struct dog *d)
 {
-if (d == 0 )
+	if (d == 0 )
 	{
-return; 
+		return; 
 	}
-else
+	else
 	{
 
-if (d->name == NULL)
-printf("nil\n");
-else
-printf("Name: %s\n", d->name);
+		if (d->name == NULL)
+		printf("nil\n");
+		else
+		printf("Name: %s\n", d->name);
 
-printf("Age: %f\n", d->age);
+		printf("Age: %f\n", d->age);
 
-if (d->owner == NULL)
-printf("nil\n");
-else
-printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+		printf("nil\n");
+		else
+		printf("Owner: %s\n", d->owner);
 
-}
+	}
 }
